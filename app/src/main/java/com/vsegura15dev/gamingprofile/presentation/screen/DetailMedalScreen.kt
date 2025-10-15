e4c8320ae3074c232ee23c4c8caf44e8f4696f8b
Victor Segura <vsegura15dev@gmail.com>
2025-10-15 17:27:32 -0500

    feature: UI - adding counter roll animation

--- app/src/main/java/com/vsegura15dev/gamingprofile/presentation/screen/DetailMedalScreen.kt
+++ app/src/main/java/com/vsegura15dev/gamingprofile/presentation/screen/DetailMedalScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -20,6 +21,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,13 +34,17 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.vsegura15dev.gamingprofile.R
 import com.vsegura15dev.gamingprofile.presentation.model.MedalUI
+import kotlinx.coroutines.delay
 
 @Composable
 fun DetailMedalScreen(
     modifier: Modifier = Modifier,
     medal: MedalUI,
+    showCounterRoll: Boolean = false,
     onCTAClicked: () -> Unit = {},
 ) {
     Box(
@@ -61,7 +69,7 @@ fun DetailMedalScreen(
                     painter = painterResource(id = medal.icon),
                     contentDescription = medal.name,
                     modifier = Modifier
-                        .size(128.dp)
+                        .size(256.dp)
                         .background(
                             Color(0xFF2C2C2E),
                             shape = MaterialTheme.shapes.medium
@@ -111,6 +119,10 @@ fun DetailMedalScreen(
                 }
             }
 
+            Text(modifier = Modifier.wrapContentSize(),
+                text = "Puntos: ${medal.points}",
+                style = MaterialTheme.typography.headlineMedium)
+
             Button(
                 onClick = onCTAClicked,
                 colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
@@ -127,6 +139,42 @@ fun DetailMedalScreen(
                 )
             }
         }
+
+        if (showCounterRoll) {
+            CounterRollDialog()
+        }
+    }
+}
+
+@Composable
+private fun CounterRollDialog() {
+    Dialog(
+        onDismissRequest = {}, properties = DialogProperties(
+            dismissOnBackPress = false,
+            dismissOnClickOutside = false
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .width(120.dp)
+                .background(Color.White, shape = RoundedCornerShape(10.dp))
+                .padding(20.dp),
+            contentAlignment = Alignment.Center
+        ) {
+
+            val number = remember { mutableIntStateOf(0) }
+            LaunchedEffect(Unit) {
+                repeat(200) {
+                    number.intValue = (1..100).random()
+                    delay(50)
+                }
+            }
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = number.intValue.toString(),
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
     }
 }
 
@@ -135,6 +183,7 @@ fun DetailMedalScreen(
 fun DetailMedalScreenPreview() {
     MaterialTheme {
         DetailMedalScreen(
+            showCounterRoll = false,
             medal = MedalUI(
                 id = "m9",
                 name = "Racha Imparable",
